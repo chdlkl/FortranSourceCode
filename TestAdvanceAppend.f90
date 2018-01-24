69c083457e851504e943c0fc5abb2e84d0c375e0
chdlkl <2016126001@chd.edu.cn>
2018-01-24 14:59:00 +0800

    update TestAdvanceAppend.f90

--- TestAdvanceAppend.f90
+++ TestAdvanceAppend.f90
@@ -4,11 +4,15 @@ Program main
   
   Open(10,file='shujv.txt',status='unknown')
   Do i=1,10
-    Write(10,'(I3)',advance='no') i  !//本应该按列输出的数据输出称为一行
+    !Write(10,'(I3)',advance='no') i  !//本应该按列输出的数据输出成为一行
+    Write( 10, '(I3\)' ) i  !// 这两句等效
   End Do
+  Write(10,'(/,\)')
+  Write(10,*) "luk"
+  Write(10,*) "luk"
   Close(10)
   
   Open(10,file='shujv.txt',status='unknown',position='append')
-  Write(10,'(A3)') 'lkl'
+  Write(10,*) 'lkl'
   Close(10)
 End Program main
